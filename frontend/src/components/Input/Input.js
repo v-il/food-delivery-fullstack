f094d37987b842046f48864d44e7a5c16b92b17d
WhatIsLoveSib <fisherfawer@gmail.com>
2023-05-22 15:31:43 +0700

    Формы на фронте переделал для DaData

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -1,8 +1,18 @@
-const Input = ({value, onChange, placeholder, className}) => {
-    const classes = className ? `p-2.5 rounded-xl border-2 ${className}` : "p-2.5 rounded-xl border-2"
-    return (
-        <input className={classes} onChange={onChange} value={value} placeholder={placeholder}/>
-    )
-}
+
+const Input = ({ value, onChange, placeholder, className, onBlur }) => {
+  const classes = className
+    ? `p-2.5 rounded-xl border-2 ${className}`
+    : "p-2.5 rounded-xl border-2";
+
+  return (
+    <input
+      className={classes}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      onBlur={onBlur}
+    />
+  );
+};
 
 export default Input;
