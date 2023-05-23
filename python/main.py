6cdaa8ae642dcf26339dc3ff1d5ee0a6a3544f6c
BezlepkinAV <a.bezlepkin@csc.sibsutis.ru>
2023-05-23 10:06:54 +0700

    Шаблоны остальных кнопок меню.

--- python/main.py
+++ python/main.py
@@ -8,7 +8,11 @@ bot = telebot.TeleBot('6092271983:AAGxi1aqDoqPkgNRLu5SugSF4WeeN42ZGas')
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     auth_button = telebot.types.KeyboardButton('Авторизоваться')
-    markup.add(auth_button)
+    # catalog_button = telebot.types.KeyboardButton('/products') Шаблоны кнопок
+    # orders_button = telebot.types.KeyboardButton('/orders')
+    # cart_button = telebot.types.KeyboardButton('/cart')
+    # markup.add(auth_button, catalog_button, orders_button, cart_button)
+    markup.add(auth_buttonn)
     bot.send_message(message.chat.id, 'Привет! Нажми на кнопку ниже, чтобы авторизоваться.', reply_markup=markup)
 
 
@@ -32,6 +36,20 @@ def auth(message):
 
     except requests.RequestException:
         bot.send_message(message.chat.id, 'Произошла ошибка при подключении к серверу')
+
+
+# @bot.message_handler(func=lambda message: message.text == 'products') если так, то в клавиатуре заменить /products на products
+# @bot.message_handler(commands=['products'])  если так, то ничего менять не нужно
+# def show_catalog(message):
+#     bot.reply_to(message, "Here is the catalog.")
+
+# @bot.message_handler(commands=['orders'])
+# def show_orders(message):
+#     bot.reply_to(message, "Here are your orders.")
+
+# @bot.message_handler(commands=['cart'])
+# def show_cart(message):
+#     bot.reply_to(message, "Here is your cart.")
 
 
 bot.polling()
